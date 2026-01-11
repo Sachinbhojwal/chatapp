09750698cc009e886ff043b77b5a3e28f7cb857e
Sachin Gupta <skguptaq13@gmail.com>
2026-01-11 18:22:01 +0530

    some update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA-FDQrMOVZsv_qt_caLmpPC-lMmfEt01o",
   authDomain: "chat-94d7c.firebaseapp.com",
+  databaseURL: "https://chat-94d7c-default-rtdb.asia-south2.firebasedatabase.app",
   projectId: "chat-94d7c",
   storageBucket: "chat-94d7c.appspot.com",
   messagingSenderId: "138739025351",
-  appId: "1:138739025351:web:4ef0bbd112d5a303a945e2",
+  appId: "1:138739025351:web:4ef0bbd112d5a303a945e2"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getDatabase(app); // Realtime Database
